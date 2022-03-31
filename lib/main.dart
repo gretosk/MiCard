61ad73c1be4bf1896239f6c0c324232662c8913f
gretosk <101219020+gretosk@users.noreply.github.com>
2022-03-31 15:08:08 +0000

    project update

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,16 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Center(child: Text('I am poor')),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+              image: AssetImage('images/iampoor.JPG')
+          ),
+        ),
       ),
     ),
   );
